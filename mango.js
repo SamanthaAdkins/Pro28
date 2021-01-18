0b3d7debfa5a963ee271033e9674c2ba4864f2a4
SamanthaAdkins <75395786+SamanthaAdkins@users.noreply.github.com>
2021-01-18 15:31:51 -0600

    Update mango.js

--- mango.js
+++ mango.js
@@ -8,7 +8,7 @@ class mango{
 		this.x = x;
 		this.y = y;
 		this.r = r;
-		this.image = loadImage("Plucking mangoes/mango.png");
+		this.image = loadImage("mango.png");
 		this.body = Bodies.circle(this.x, this.y, this.r, options);
 		World.add(world, this.body);
 	}
